edcefdf6f44fca01f1ce0cd9a9cae6293e924402
mrmnmst <meirman@mail.ru>
2025-03-25 23:16:22 +0500

    Сохранили изменения перед git pull

--- Users/meirm/flutterflow25/norma_vyseva_s_k_p2_1/lib/custom_code/actions/index.dart
+++
@@ -1,6 +0,0 @@
-export 'ca_otkr_katush.dart' show caOtkrKatush;
-export 'new_custom_action_gektary_na_bochku.dart'
-    show newCustomActionGektaryNaBochku;
-export 'new_custom_action_v_odnom_m_m.dart' show newCustomActionVOdnomMM;
-export 'new_custom_action_dolivka_bochki.dart'
-    show newCustomActionDolivkaBochki;
--- Users/meirm/flutterflow25/norma_vyseva_s_k_p2_1/lib/index.dart
+++
@@ -1,4 +0,0 @@
-// Export pages
-export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
-export '/pages/home_page_kaz/home_page_kaz_widget.dart' show HomePageKazWidget;
-export '/pages/home_page_rus/home_page_rus_widget.dart' show HomePageRusWidget;
